93f470ea226d4c8a9a3612cdd69429505035bce1
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-07-28 21:06:55 +0700

    chore: ignore backend/dist and stop tracking compiled output

--- backend/dist/modules/certificates/certificate.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const certificate_controller_1 = require("./certificate.controller");
-const router = (0, express_1.Router)();
-// Public verification endpoint
-router.get('/:code', certificate_controller_1.getCertificateByCode);
-exports.default = router;
